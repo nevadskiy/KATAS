df49af55ca60f7579fc16fb03d81e47ea5f4cbc1
Nevadskiy <nevadskiy@gmail.com>
2018-06-23 13:29:08 +0300

    Feature prime factor generator

--- prime-factors/src/PrimeFactor.php
+++ prime-factors/src/PrimeFactor.php
@@ -4,6 +4,19 @@ class PrimeFactor
 {
     public function generate($number)
     {
-        return [];
+        $primes = [];
+        $divisor = 2;
+        
+        while ($number > 1) {
+            while ($number % $divisor === 0) // Non-recursive approach
+            {
+                $primes[] = $divisor;
+                $number /= $divisor;
+            }
+            
+            $divisor++;
+        }
+        
+        return $primes;
     }
 }
--- prime-factors/tests/PrimeFactorTest.php
+++ prime-factors/tests/PrimeFactorTest.php
@@ -19,6 +19,62 @@ class PrimeFactorTest extends TestCase
     {
         $result = $this->primeFactor->generate(1);
         
-        $this->assertEquals($result, []);
+        $this->assertEquals([], $result);
+    }
+    
+    /** @test */
+    public function it_retunrs_2_for_2()
+    {
+        $result = $this->primeFactor->generate(2);
+        
+        $this->assertEquals([2], $result);
+    }
+    
+    /** @test */
+    public function it_returns_3_for_3()
+    {
+        $result = $this->primeFactor->generate(3);
+    
+        $this->assertEquals([3], $result);
+    }
+    
+    /** @test */
+    public function it_returns_2_2_for_4()
+    {
+        $result = $this->primeFactor->generate(4);
+        
+        $this->assertEquals([2, 2], $result);
+    }
+    
+    /** @test */
+    public function it_returns_5_for_5()
+    {
+        $result = $this->primeFactor->generate(5);
+        
+        $this->assertEquals([5], $result);
+    }
+    
+    /** @test */
+    public function it_returns_2_3_for_6()
+    {
+        $result = $this->primeFactor->generate(6);
+        
+        $this->assertEquals([2, 3], $result);
+    }
+    
+    /** @test */
+    public function it_returns_2_2_2_for_8()
+    {
+        $result = $this->primeFactor->generate(8);
+        
+        $this->assertEquals([2, 2, 2], $result);
+    }
+    
+    /** @test */
+    public function it_returns_3_3_for_9()
+    {
+        $result = $this->primeFactor->generate(9);
+        
+        $this->assertEquals([3, 3], $result);
     }
 }
